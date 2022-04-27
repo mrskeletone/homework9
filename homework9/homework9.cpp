d74be1bdabad6896bbec631c381f4d16bac1898b
mrskeletone <mr_skeletone_1000@mail.ru>
2022-04-27 20:43:00 +0300

    1

--- homework9/homework9.cpp
+++ homework9/homework9.cpp
@@ -5,6 +5,7 @@ class Property
 public:
 	Property(double a) { worth = a; }
 	virtual double tax() { return 0; }
+	virtual ~Property() {}
 protected:
 	double worth;
 };
@@ -31,19 +32,21 @@ private:
 int main()
 {
 	Property* taxn[7];
-	//double tax_a1=1000, tax_a2=2000, tax_a3=3000, tax_c1=200, tax_c2=400, tax_co1=500, tax_co2=1000;
-	Apartment tax_a1 ( 1000), tax_a2 ( 2000), tax_a3(3000);
-	Car tax_c1 ( 200), tax_c2 ( 400);
-	CountryHouse tax_co1 ( 500), tax_co2 ( 1000);
-	taxn[0] = &tax_a1;
-	taxn[1] = &tax_a2;
-	taxn[2] = &tax_a3;
-	taxn[3] = &tax_c1;
-	taxn[4] = &tax_c2;
-	taxn[5] = &tax_co1;
-	taxn[6] = &tax_co2;
+	
+	taxn[0] = new Apartment(1000);
+	taxn[1] = new Apartment(2000);
+	taxn[2] = new Apartment(3000);
+	taxn[3] = new Car(200);
+	taxn[4] = new Car(400);
+	taxn[5] = new CountryHouse(500);
+	taxn[6] = new CountryHouse(1000);
 	for (int i = 0; i < 7; i++) {
 		cout << taxn[i]->tax() << endl;
 	}
+	for (int i = 0; i < 7; i++) {
+		delete taxn[i];
+	}
 }
+
+
 
